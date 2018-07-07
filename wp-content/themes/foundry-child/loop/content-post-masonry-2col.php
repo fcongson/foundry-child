f6cce2bafc3ea83d6216c4b83d585ac18eb642b8
Frank Congson <fcongson@gmail.com>
2018-07-07 17:26:12 +1200

    Clean up post masonry layout

---
+++ wp-content/themes/foundry-child/loop/content-post-masonry-2col.php
@@ -0,0 +1,29 @@
+<?php $format = get_post_format(); ?>
+
+<!-- <div class="col-sm-6 post-snippet masonry-item"> -->
+<div class="col-sm-6 post-snippet">
+
+    <?php get_template_part('inc/content-format', $format); ?>
+    
+    <?php if(!( 'quote' == $format )) : ?>
+	    <div class="inner">
+	    
+	        <a href="<?php the_permalink(); ?>">
+	            <?php the_title('<h6 class="mb0 uppercase">', '</h6><span class="inline-block mb16">'. get_the_time(get_option('date_format')) .'</span>'); ?>
+	        </a>
+	        
+	        <!-- <hr>
+	        
+	        <?php 
+	        	if(!( 'page' == get_post_type() ))
+	        		the_excerpt(); 
+	        ?>
+	        
+	        <a class="btn btn-sm" href="<?php the_permalink(); ?>"><?php _e('Read More','foundry'); ?></a>
+	        
+	        <?php get_template_part('inc/content','post-sharing'); ?> -->
+	        
+	    </div>
+	<?php endif; ?>
+    
+</div>
